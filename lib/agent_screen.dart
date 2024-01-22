89c44342e37e3d0d7fdd9855b44582c5002c34bb
BuildDeWurld <1inamilionz@gmail.com>
2024-01-22 11:57:47 +0100

    second commit

--- lib/agent_screen.dart
+++ lib/agent_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'nav_drawer.dart';
+
 class Agent extends StatefulWidget {
   const Agent({super.key});
 
@@ -9,11 +11,7 @@ class Agent extends StatefulWidget {
 
 class _AgentState extends State<Agent> {
   bool showBalance = true;
-  // @override
-  // void initState() {
-  //   // TODO: implement initState
-  //   super.initState();
-  // }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -87,88 +85,15 @@ class _AgentState extends State<Agent> {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Container(
-                      height: 150,
-                      width: 150,
-                      decoration: BoxDecoration(
-                          color: Colors.grey,
-                          borderRadius: BorderRadius.circular(20)),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(Icons.mouse),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Text("Cash Out")
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Container(
-                        height: 150,
-                        width: 150,
-                        decoration: BoxDecoration(
-                            color: Colors.grey,
-                            borderRadius: BorderRadius.circular(20)),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(Icons.mouse),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            Text("Cash Out")
-                          ],
-                        ),
-                      ),
-                    ),
+                    AgentContainer(),
+                    AgentContainer(),
                   ],
                 ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Container(
-                        height: 150,
-                        width: 150,
-                        decoration: BoxDecoration(
-                            color: Colors.grey,
-                            borderRadius: BorderRadius.circular(20)),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(Icons.mouse),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            Text("Cash Out")
-                          ],
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Container(
-                        height: 150,
-                        width: 150,
-                        decoration: BoxDecoration(
-                            color: Colors.grey,
-                            borderRadius: BorderRadius.circular(20)),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(Icons.mouse),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            Text("Cash Out")
-                          ],
-                        ),
-                      ),
-                    ),
+                    AgentContainer(),
+                    AgentContainer(),
                   ],
                 )
               ],
@@ -180,71 +105,30 @@ class _AgentState extends State<Agent> {
   }
 }
 
-// class BalanceWid extends StatefulWidget {
-//   const BalanceWid({super.key, required this.showBalance});
-
-//   final bool showBalance;
-
-//   @override
-//   State<BalanceWid> createState() => _BalanceWidState();
-// }
-
-// class _BalanceWidState extends State<BalanceWid> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return widget.showBalance
-//         ? Text("  N1234567890  ",
-//             style: TextStyle(color: Colors.white, fontSize: 18))
-//         : Text("  N ********** ",
-//             style: TextStyle(color: Colors.white, fontSize: 18));
-//   }
-// }
-
-class NavDrawer extends StatelessWidget {
-  const NavDrawer({super.key});
+class AgentContainer extends StatelessWidget {
+  const AgentContainer({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      backgroundColor: Colors.white,
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: <Widget>[
-          DrawerHeader(
-            child: Text(
-              'Side menu',
-              style: TextStyle(color: Colors.white, fontSize: 25),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        height: 150,
+        width: 150,
+        decoration: BoxDecoration(
+            color: Colors.grey, borderRadius: BorderRadius.circular(20)),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.mouse),
+            SizedBox(
+              height: 10,
             ),
-            decoration: BoxDecoration(
-              color: Colors.purple,
-            ),
-          ),
-          ListTile(
-            leading: Icon(Icons.input),
-            title: Text('Welcome'),
-            onTap: () => {},
-          ),
-          ListTile(
-            leading: Icon(Icons.verified_user),
-            title: Text('Profile'),
-            onTap: () => {Navigator.of(context).pop()},
-          ),
-          ListTile(
-            leading: Icon(Icons.settings),
-            title: Text('Settings'),
-            onTap: () => {Navigator.of(context).pop()},
-          ),
-          ListTile(
-            leading: Icon(Icons.border_color),
-            title: Text('Feedback'),
-            onTap: () => {Navigator.of(context).pop()},
-          ),
-          ListTile(
-            leading: Icon(Icons.exit_to_app),
-            title: Text('Logout'),
-            onTap: () => {Navigator.of(context).pop()},
-          ),
-        ],
+            Text("Cash Out")
+          ],
+        ),
       ),
     );
   }
---
+++ lib/nav_drawer.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class NavDrawer extends StatelessWidget {
+  const NavDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: Colors.white,
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Text(
+              'Side menu',
+              style: TextStyle(color: Colors.white, fontSize: 25),
+            ),
+            decoration: BoxDecoration(
+              color: Colors.purple,
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.input),
+            title: Text('Welcome'),
+            onTap: () => {},
+          ),
+          ListTile(
+            leading: Icon(Icons.verified_user),
+            title: Text('Profile'),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+          ListTile(
+            leading: Icon(Icons.settings),
+            title: Text('Settings'),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+          ListTile(
+            leading: Icon(Icons.border_color),
+            title: Text('Feedback'),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+          ListTile(
+            leading: Icon(Icons.exit_to_app),
+            title: Text('Logout'),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+        ],
+      ),
+    );
+  }
+}
